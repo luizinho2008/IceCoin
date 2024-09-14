0f6833427beba8e7a70aef48525568b0cc4f6a00
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-09-14 10:47:49 -0300

    Definição da quantidade de IceCoins existentes e o valor em R$ de 1 IceCoin

--- src/IceCoin.java
+++ src/IceCoin.java
@@ -1,4 +1,6 @@
 public class IceCoin {
-    static Double valor = 20000000.0;
+
+    static Double valor = 250000000.00; // de reais
+    static Double quantidade = 550000000.00; //de icecoins existentes
     
 }
