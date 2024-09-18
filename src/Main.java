1f46d3d4583431c0b4a471cbba91dcb19a6be59b
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-09-18 16:17:12 -0300

    Remoção dos códigos da Blockchain na classe principal

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 // Classe principal para testar a blockchain
 public class Main {
     public static void main(String[] args) {
-        Blockchain minhaBlockchain = new Blockchain();
+        // Blockchain minhaBlockchain = new Blockchain();
 
-        minhaBlockchain.adicionarBloco("Primeiro bloco de dados");
-        minhaBlockchain.adicionarBloco("Segundo bloco de dados");
-        minhaBlockchain.adicionarBloco("Terceiro bloco de dados");
-        minhaBlockchain.adicionarBloco("Quarto bloco de dados");
+        // minhaBlockchain.adicionarBloco("Primeiro bloco de dados");
+        // minhaBlockchain.adicionarBloco("Segundo bloco de dados");
+        // minhaBlockchain.adicionarBloco("Terceiro bloco de dados");
+        // minhaBlockchain.adicionarBloco("Quarto bloco de dados");
 
-        System.out.println("Blockchain:");
-        minhaBlockchain.exibirBlockchain();
+        // System.out.println("Blockchain:");
+        // minhaBlockchain.exibirBlockchain();
 
-        System.out.println("Integridade da Blockchain: " + minhaBlockchain.verificarIntegridade());
+        // System.out.println("Integridade da Blockchain: " + minhaBlockchain.verificarIntegridade());
     }
 }
