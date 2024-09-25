c43d045828a71d78139ff99ed3a6847c276b21c9
Vini-Sauer <viniotsa@gmail.com>
2024-09-25 17:25:22 -0300

    adição do cadastro e da simulação

--- src/IceCoin.java
+++ src/IceCoin.java
@@ -1,6 +1,6 @@
 public class IceCoin {
 
-    static Double valor = 250000000.00; // de reais
-    static Double quantidade = 550000000.00; //de icecoins existentes
+    static Double valor = 1000000.00; // de reais
+    static Double quantidade = 5500.00; //de icecoins existentes
     
 }
