4a95ebc44a26c61ac3c699f2eeb315c9044f802b
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-10-02 09:42:27 -0300

    Tentativa da criação de cadastro

--- src/Controller3.java
+++ src/Controller3.java
@@ -5,9 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.util.HashMap;
 
 public class Controller3 {
+
+    static HashMap<String, String> usuarios = new HashMap<>();
+
+    @FXML
+    private TextField cSenhaInput;
+
+    @FXML
+    private TextField senhaInput;
+
+    @FXML
+    private TextField usuarioInput;
+
     @FXML
     void redirecionarLogin(ActionEvent event) {
         try {
@@ -31,6 +45,19 @@ public class Controller3 {
             palco.show();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void cadastrar() {
+        if (senhaInput.getText().equals(cSenhaInput.getText()) && 
+            !usuarioInput.getText().isEmpty() && 
+            !usuarios.containsKey(usuarioInput.getText())) {
+            
+            usuarios.put(usuarioInput.getText(), senhaInput.getText());
+            System.out.println("Cadastrado com sucesso no IceCoin");
+        } else {
+            System.out.println("Falha ao cadastrar usuário no IceCoin");
         }
     }
 }
