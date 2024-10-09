7e1d7f01724e8abe6fc3587b02bca00afb0cc453
Luis-Vansan <luquevansan@gmail.com>
2024-10-09 17:02:38 -0300

    ajustando a pagina login

--- src/ControllerLogin.java
+++ src/ControllerLogin.java
@@ -6,6 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -14,7 +15,7 @@ import javafx.stage.Stage;
 public class ControllerLogin {
 
     @FXML
-    private TextField senhaInput;
+    private PasswordField senhaInput;
 
     @FXML
     private TextField usuarioInput;
