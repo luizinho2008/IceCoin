b279218ec9bd11fd2439f496d63be597f96eb303
Pedro-Miguel-S <pedromiguelsarur@gmail.com>
2024-10-09 17:46:57 -0300

    fazendo o conversor funcionar e mudando tambem a tela de simulacao

--- src/ControllerSimulacao.java
+++ src/ControllerSimulacao.java
@@ -1,4 +1,36 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ControllerSimulacao {
+
+    @FXML
+    private Label converterValor;
+
+    @FXML
+    private TextField valorReal;
+
+    @FXML
+    void converter(ActionEvent event) {
+
+        String valor = valorReal.getText();
+
+        try {
+            Double convertido = Double.parseDouble(valor);
+            
+            Double valorMoeda = IceCoin.getValor();
+
+            convertido = convertido / valorMoeda;
+
+            valor = String.valueOf(convertido);
+
+            converterValor.setText(valor);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Erro: O texto inserido não é um número válido.");
+        }
+
+    }
 
 }
--- src/IceCoin.java
+++ src/IceCoin.java
@@ -1,6 +1,14 @@
 public class IceCoin {
 
-    static Double valor = 1000000.00; // de reais
+    static Double valor = 25.00; // de reais
     static Double quantidade = 5500.00; //de icecoins existentes
+    
+    public static Double getValor() {
+        return valor;
+    }
+    public static Double getQuantidade() {
+        return quantidade;
+    }
+    
     
 }
