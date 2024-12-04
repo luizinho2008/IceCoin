189481e9295501bf8217d672d5eeaf1e8d86283e
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-12-04 17:54:29 -0300

    Validando envio de IceCoin's entre mesmos endereços

--- src/ControllerTransferir.java
+++ src/ControllerTransferir.java
@@ -9,6 +9,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -99,6 +101,15 @@ public class ControllerTransferir {
 
         System.out.println("Remetente: " + Remetente);
         System.out.println("Destinatário: " + Destinatario);
+
+
+        if(Remetente.equals(Destinatario)) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Falha na transferência");
+            alert.setHeaderText(null);
+            alert.setContentText("Destinatário inválido!");
+            alert.showAndWait();
+        }
     }
 
 }
