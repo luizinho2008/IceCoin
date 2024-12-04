9e08b68171ecd0987722d36d1366ba75075730fe
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-12-04 17:01:27 -0300

    Combo box pronto com os meus hashs

--- src/ControllerTransferir.java
+++ src/ControllerTransferir.java
@@ -1,15 +1,29 @@
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ControllerTransferir {
+
+    @FXML
+    private TextField destinatario;
+
+    @FXML
+    private TextField quantidade;
+
+    @FXML
+    private ComboBox<String> remetente;
 
     @FXML
     private Text carteira_menuBar;
@@ -19,6 +33,33 @@ public class ControllerTransferir {
 
     private Stage stage;
     private Scene scene;
+
+    private Connection connectToDatabase() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/icecoin_db";
+        String user = "root";
+        String password = "ifsp";
+    
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    @FXML
+    private void initialize() {
+        try (Connection connection = connectToDatabase()) {
+            String query = "SELECT * FROM contas WHERE id_usuario = ?";
+            var preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, Sessao.getIdUsuario());
+            
+            var resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String nomeRemetente = resultSet.getString("endereco");
+                remetente.getItems().add(nomeRemetente);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     void irParaCarteira(MouseEvent event) {
