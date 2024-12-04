3dc60d440972e9fcbb92a29189e165ef42b16209
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-12-04 17:23:20 -0300

    Função de copiar o hash

--- src/ControllerGerarEndereco.java
+++ src/ControllerGerarEndereco.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,7 +20,7 @@ public class ControllerGerarEndereco {
     private Text contato_menuBar;
 
     @FXML
-    private Text endereco;
+    private TextField endereco;
 
     @FXML
     private Text carteira_menuBar;
