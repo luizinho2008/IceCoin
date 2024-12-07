5565f98710436af383d9a488892d7e9380dbe618
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-12-07 15:44:17 -0300

    100% pronto IceCoin

--- src/MySQL.java
+++ src/MySQL.java
@@ -1,7 +1,7 @@
 public class MySQL {
-    private static String url = "jdbc:mysql://localhost:3306/icecoin_db";
-    private static String user = "root";
-    private static String password = "";
+    private static String url = "jdbc:mysql://nx6vj.h.filess.io:3307/IceCoin_betweenfox";
+    private static String user = "IceCoin_betweenfox";
+    private static String password = "186f084cc6f22a27c5ff1ea3faf39e85896fb47e";
 
 
     public static String getUrl() {
